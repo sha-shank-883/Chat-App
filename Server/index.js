bdf20e9b59f8315821802461bbb7d57ca6848980
shashank singhal <shashanksinghal883@gmail.com>
2022-09-18 22:50:08 +0530

    merge CLient folder in Server

--- Server/index.js
+++ Server/index.js
@@ -6,6 +6,7 @@ const messageRoute = require("./routes/messagesRoute");
 const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -25,9 +26,13 @@ mongoose
     console.log(err.message);
   });
 
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Server started on ${process.env.PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("Client/build"));
+}
 
 const io = socket(server, {
   cors: {
